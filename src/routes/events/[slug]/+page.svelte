9b3187ae42882985c0e72e0b6f18312563a7fccf
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-21 16:09:48 -0700

    unsucessful debugging

--- src/routes/events/[slug]/+page.svelte
+++ src/routes/events/[slug]/+page.svelte
@@ -1,6 +1,12 @@
 <script>
 	export let data;
 </script>
+<!-- 
+there must be something wrong with the way i am calling / loading the data -->
+<svelte:head>
+	<title>{data.post.title}</title>
+</svelte:head>
 
 <h1>{data.post.title}</h1>
+<p>{data.post.content}</p>
 <!-- <div>{data.post.content}</div> -->
