b2f3900cc1e716424c5123b624eb1c4b7a705395
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-23 14:27:23 -0700

    moved gallery from home page to events page, kept code on homepage in comments

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,8 +1,39 @@
 <script>
   export let data;
+  import Card from '../../lib/components/Card.svelte';
 </script>
 
-{#each data.summaries as { slug, title }}
-<li><a href="./events/{slug}">{title}</a></li>
-{/each}
+<svelte:head>
+  <title>Gallery Page</title>
+</svelte:head>
+<main>
+<div class="card-container">
 
+  <!-- each loop for card to display events  -->
+  {#each data.summaries as { slug, title, content, src } }
+    <li><a href="./events/{slug}"><Card {title} {content} {src}/></a></li>
+    <!-- <Card title={title} content={content} src={src} /> -->
+  {/each}
+  
+</div>
+</main>
+
+<style>
+  .card-container {
+    padding: 5rem;
+    display: flex;
+    justify-content: space-evenly; /* or space-between */
+    flex-wrap: wrap;
+    max-width: 1000px; /* Adjust as needed */
+    margin: 0 auto; /* Center the cards */
+  }
+  li {
+    list-style-type: none;
+  }
+  a{
+    text-decoration: none;
+  }
+
+</style>
+  
+  
