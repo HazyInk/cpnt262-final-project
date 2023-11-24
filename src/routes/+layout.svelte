7e1c9277f3a63f62dd051e838e32a919deddfbc4
HazyInk <reillysheapaston@gmail.com>
2023-11-24 09:40:18 -0700

    added content into menu/+page.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,10 @@
+<script>
+  import Footer from "../lib/components/Footer.svelte";
+  import NavBar from "../lib/components/NavBar.svelte";
+</script>
 
+<NavBar></NavBar>
+
+<slot />
+
+<Footer></Footer>
